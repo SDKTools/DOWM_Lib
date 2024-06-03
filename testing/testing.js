d728ae98f270f7b4011f6a279ee20f9805a79b65
SDKTools <jonaslan495@gmail.com>
2024-06-03 13:35:22 -0300

    Negative numbers doens't work

--- testing/testing.js
+++ testing/testing.js
@@ -1,6 +1,6 @@
 const { DOWM } = require("dowm_lib");
 
-let dowmIA = new DOWM(3, [0.76, 0.89, -0.94]); // Brilho, Saturação, Rugosidade(em medida)
+let dowmIA = new DOWM(3, [0.76, 0.89, 0.94]); // Brilho, Saturação, Liso(em medida)
 
 let result = dowmIA.run([0.89, 0.90, 0.90]);
 
